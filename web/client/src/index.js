b2350ed0cf4ef12fd0f535e3d97a4e60a1a90205
andyliu-417 <andy2018.liu@gmail.com>
2018-05-15 22:01:41 -0400

    restructure

--- web/client/src/index.js
+++ web/client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
