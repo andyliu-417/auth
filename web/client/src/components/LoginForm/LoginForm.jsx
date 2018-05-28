51a49e038972e006e973ffbd94a94b7bf7be1e8a
andyliu-417 <andy2018.liu@gmail.com>
2018-05-27 22:46:45 -0400

    loginform function

--- web/client/src/components/LoginForm/LoginForm.jsx
+++ web/client/src/components/LoginForm/LoginForm.jsx
@@ -2,62 +2,56 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-class LoginForm extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-  }
+function LoginForm({ onSubmit, onChange }) {
+  return (
+    <div className="container">
+      <div className="card-panel login-panel">
+        <form className="col s12" action="/" onSubmit={onSubmit}>
+          <h4 className="center-align">Login</h4>
 
-  render() {
-    return (
-      <div className="container">
-        <div className="card-panel login-panel">
-          <form className="col s12" action="/" onSubmit={this.props.onSubmit}>
-            <h4 className="center-align">Login</h4>
-
-            <div className="row">
-              <div className="input-field col s12">
-                <input
-                  className="validate"
-                  id="email"
-                  type="email"
-                  name="email"
-                  onChange={this.props.onChange}
-                />
-                <label htmlFor="email">Email</label>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="input-field col s12">
-                <input
-                  className="validate"
-                  id="password"
-                  type="password"
-                  name="password"
-                  onChange={this.props.onChange}
-                />
-                <label htmlFor="password">Password</label>
-              </div>
-            </div>
-
-            <div className="row right-align">
+          <div className="row">
+            <div className="input-field col s12">
               <input
-                type="submit"
-                className="waves-effect waves-light btn indigo lighten-1"
-                value="Log in"
+                className="validate"
+                id="email"
+                type="email"
+                name="email"
+                onChange={onChange}
               />
+              <label htmlFor="email">Email</label>
             </div>
+          </div>
 
-            <div className="row">
-              <p className="right-align">
-                New to here? <Link to="/signup">Sign Up</Link>
-              </p>
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                className="validate"
+                id="password"
+                type="password"
+                name="password"
+                onChange={onChange}
+              />
+              <label htmlFor="password">Password</label>
             </div>
-          </form>
-        </div>
+          </div>
+
+          <div className="row right-align">
+            <input
+              type="submit"
+              className="waves-effect waves-light btn indigo lighten-1"
+              value="Log in"
+            />
+          </div>
+
+          <div className="row">
+            <p className="right-align">
+              New to here? <Link to="/signup">Sign Up</Link>
+            </p>
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default LoginForm;
--- web/client/src/containers/Login/Login.jsx
+++ web/client/src/containers/Login/Login.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 
-import './Login.css';
-import LoginForm from '../../components/LoginForm/LoginForm';
+import "./Login.css";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 class Login extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleSubmit() {}
+
+  handleChange() {}
+
   render() {
-  return (
-    <div className="login-header">
-        <LoginForm />
-    </div>);
+    return (
+      <div className="login-header">
+        <LoginForm onSubmit={this.handleSubmit} onChange={this.handleChange} />
+      </div>
+    );
   }
 }
 
