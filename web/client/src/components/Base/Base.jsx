6a73d45f5e7256bdf69f47a2122833a6438a3aa5
andyliu-417 <andy2018.liu@gmail.com>
2018-05-29 16:27:26 -0400

    Base

---
+++ web/client/src/components/Base/Base.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router";
+import Auth from "../Auth/Auth";
+import "./Base.css";
+
+export default function Base() {
+  return (
+    <div>
+      <nav className="nav-bar indigo lighten-1">
+        <div className="nav-wrapper">
+          <a href="/" className="brand-logo">
+            Base
+          </a>
+          <ul id="nav-mobile" className="right">
+            {Auth.isUserAuthenticated() ? (
+              <div>
+                <li>{Auth.getEmail()}</li>
+                <li>
+                  <Link to="/logout">Log out</Link>
+                </li>
+              </div>
+            ) : (
+              <div>
+                <li>
+                  <Link to="/login">Log in</Link>
+                </li>
+                <li>
+                  <Link to="/signup">Sign up</Link>
+                </li>
+              </div>
+            )}
+          </ul>
+        </div>
+      </nav>
+      <br />
+    </div>
+  );
+}
